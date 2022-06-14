bdef3f34b3da59ec68ae74b100f9847f5c76a550
Xavimat <xaviermatoses@gmail.com>
2022-06-14 13:41:04 +0200

    Person navbar style

--- src/components/Personal/Personal.jsx
+++ src/components/Personal/Personal.jsx
@@ -1,11 +1,7 @@
 import PersNav from "./PersNav/PersNav"
 
 function Personal() {
-  return (
-    <div className='border border-dark rounded my-2'>
-        <PersNav/>
-    </div>
-  )
+  return (<PersNav />)
 }
 
 export default Personal
