fd2ac5b828b580ca56e83a6f09f674e9f9618789
Xavimat <xaviermatoses@gmail.com>
2022-06-14 13:57:48 +0200

    Contact Form strcuture

--- src/components/Personal/PersContact/PersContact.jsx
+++ src/components/Personal/PersContact/PersContact.jsx
@@ -1,12 +1,50 @@
 import PersNav from "../PersNav/PersNav"
 
 function PersContact() {
-    return (
-        <div>
-            <PersNav/>
-            PersContact
-        </div>
-    )
+  const onChangeHandler = (ev) => { }
+  const onSubmitHandler = (ev) => { }
+  return (
+    <div>
+      <PersNav />
+      <div className="container">
+        <h1>Contact Form</h1>
+        <form onSubmit={onSubmitHandler}>
+          <div className="d-md-flex">
+            <div class="m-3">
+              <label class="form-label">Name:</label>
+              <input
+                type="text"
+                name="name"
+                class="form-control"
+                onChange={onChangeHandler}
+                placeholder="Name" />
+            </div>
+            <div class="m-3">
+              <label class="form-label">Email:</label>
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                onChange={onChangeHandler}
+                placeholder="Email" />
+            </div>
+            <div class="m-3">
+              <label class="form-label">Password:</label>
+              <input
+                type="password"
+                name="pass"
+                class="form-control"
+                onChange={onChangeHandler}
+                placeholder="Password" />
+            </div>
+          </div>
+          <div class="m-3">
+            <input type="submit" value="Send" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default PersContact
