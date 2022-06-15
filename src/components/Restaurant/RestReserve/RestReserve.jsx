160b431d0a6639da9e011f17e1f40e2274863283
Xavimat <xaviermatoses@gmail.com>
2022-06-15 10:19:46 +0200

    Limit dates

--- src/components/Restaurant/RestReserve/RestReserve.jsx
+++ src/components/Restaurant/RestReserve/RestReserve.jsx
@@ -48,43 +48,91 @@ function RestReserve({ db }) {
           <div className="d-sm-flex">
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Name</label>
-              <input type="text" className="form-control"
-                id="form-name" name="name" onChange={handleInputChange} value={data.name} placeholder="Name" required />
+              <input
+                type="text"
+                className="form-control"
+                id="form-name"
+                name="name"
+                onChange={handleInputChange}
+                value={data.name}
+                placeholder="Name"
+                required />
             </div>
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control"
-                id="form-email" name="email" onChange={handleInputChange} value={data.email} placeholder="Email" required />
+              <input
+                type="email"
+                className="form-control"
+                id="form-email"
+                name="email"
+                onChange={handleInputChange}
+                value={data.email}
+                placeholder="Email"
+                required />
             </div>
           </div>
           <div className="d-sm-flex">
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Phone</label>
-              <input type="text" className="form-control"
-                id="form-phone" name="phone" onChange={handleInputChange} value={data.phone} placeholder="Phone" required />
+              <input
+                type="text"
+                className="form-control"
+                id="form-phone"
+                name="phone"
+                onChange={handleInputChange}
+                value={data.phone}
+                placeholder="Phone"
+                required />
             </div>
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">#of Guests</label>
-              <input type="number" className="form-control"
-                id="form-guests" name="guests" onChange={handleInputChange} value={data.guests} placeholder="#of Guests" required />
+              <input
+                type="number"
+                className="form-control"
+                id="form-guests"
+                name="guests"
+                onChange={handleInputChange}
+                value={data.guests}
+                placeholder="#of Guests"
+                required />
             </div>
           </div>
           <div className="d-sm-flex">
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Date</label>
-              <input type="date" className="form-control"
-                id="form-date" name="date" onChange={handleInputChange} value={data.date} placeholder="Date" required />
+              <input
+                type="date"
+                className="form-control"
+                id="form-date"
+                name="date"
+                onChange={handleInputChange}
+                value={data.date}
+                placeholder="Date"
+                min={new Date().toISOString().split("T")[0]}
+                required />
             </div>
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Time</label>
-              <input type="time" className="form-control"
-                id="form-time" name="time" onChange={handleInputChange} value={data.time} placeholder="Time" required />
+              <input
+                type="time"
+                className="form-control"
+                id="form-time"
+                name="time"
+                onChange={handleInputChange}
+                value={data.time}
+                placeholder="Time"
+                required />
             </div>
           </div>
           <div className="d-sm-flex">
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Reservation Type</label>
-              <select className="form-select" onChange={handleInputChange} id="form-type" name="type" required>
+              <select
+                className="form-select"
+                onChange={handleInputChange}
+                id="form-type"
+                name="type"
+                required>
                 <option value="Dinner">Dinner</option>
                 <option value="VIP">VIP</option>
                 <option value="Birthday">Birthday</option>
@@ -97,8 +145,13 @@ function RestReserve({ db }) {
             </div>
             <div className="my-3 mx-1 flex-fill">
               <label className="form-label">Any special requests</label>
-              <textarea className="form-control" onChange={handleInputChange}
-                id="form-message" name="message" value={data.message}></textarea>
+              <textarea
+                className="form-control"
+                onChange={handleInputChange}
+                id="form-message"
+                name="message"
+                value={data.message}>
+              </textarea>
             </div>
           </div>
           <div className="text-center">
